1bff46215b3f27a97ba8d6bd257a93ab7298f47b
Chet <chethana96@gmail.com>
2019-07-13 21:41:36 +1200

    added unformatted timeseries table headers

--- src/components/timeseriesdatatable.js
+++ src/components/timeseriesdatatable.js
@@ -6,14 +6,38 @@ const { Column, ColumnGroup } = Table;
 const TimeseriesDataTable = ({ data }) => {
     const totalVal = data.filter(record => record.group === "Total")[0];
     const yearsCols = Object.keys(totalVal).filter(key => key.includes("percent"));
+    const years = yearsCols.map(year => {
+        return {
+            dataYear: year,
+            colYear: Number.parseInt(`20${year.substring(year.indexOf("_")+1)}`)
+        }
+    });
 
-    console.log(yearsCols);
+    const comparisonCols = Object.keys(totalVal).filter(key => key.includes("p_value"));
+    const comparisonYears = comparisonCols.map(comp => {
+        const shortYears = comp.substring(8).split("_").reverse();
+        const longYears = shortYears.map(year => Number.parseInt(`20${year}`));
+        return {
+            dataComparison: comp,
+            colComparison: longYears.join(" and ")
+        }
+    })
+
+    console.log(data);
 
     return (
         <Table>
-            <Column title="Population group" width="30%"
+            <Column title="Population group" width="20%"
             />
             <ColumnGroup title="Unadjusted prevalence">
+                {years.map(year => (
+                    <Column title={year.colYear} key={year.dataYear} dataIndex={year.dataYear} />
+                ))}
+            </ColumnGroup>
+            <ColumnGroup title="Statistical difference between years (p value)">
+                {comparisonYears.map(comp => (
+                    <Column title={comp.colComparison} key={comp.dataComparison} dataIndex={comp.dataComparison} />
+                ))}
             </ColumnGroup>
         </Table>
     )
