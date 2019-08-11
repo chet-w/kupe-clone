1a83fcf0a95dff1970af55f900fc9c0800d3f3b8
Chet <chethana96@gmail.com>
2019-08-11 17:46:08 +1200

    added boilerplate filters form (still just a login form rn)

--- src/components/mainfilters.js
+++ src/components/mainfilters.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import { Select } from "antd";
+// import { Select } from "antd";
+import { Form, Icon, Input, Button } from 'antd';
+// const { Option } =  Select;
 
-const { Option } =  Select;
 
-const MainFilters = () => {
+const MainFilters = ({ form }) => {
+
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = form;
+
+    // Only show error after a field is touched.
+    const usernameError = isFieldTouched('username') && getFieldError('username');
+    const passwordError = isFieldTouched('password') && getFieldError('password');
+
     return (
-        <>
-            <Select defaultValue={"lucy"}>
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-            </Select>
-            {/* <Select/>
-            <Select/> */}
-        </>
-    )
+        <Form layout="inline" onSubmit={() => console.log("submitted!")}>
+          <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''}>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item validateStatus={passwordError ? 'error' : ''} help={passwordError || ''}>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      );
 }
 
-export default MainFilters;
+const Wrapped = Form.create({ name: 'horizontal_login' })(MainFilters);
+
+export default Wrapped;
--- src/components/splashexplore.js
+++ src/components/splashexplore.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import Container from './ui/container';
+import styled from "styled-components";
 import MainFilters from "./mainfilters";
+import Container from './ui/container';
+import PageHeading from "./ui/pageheading";
+
+const StyledSplashExplore = styled.div`
+    width: 100%;
+    padding: 20px 0;
+    display: flex;
+    justify-content: center;
+    border-top: solid 2px ${props => props.theme.lightGrey};
+    border-bottom: solid 2px ${props => props.theme.lightGrey};
+`;
 
 const SplashExplore = () => {
     return (
-        <Container>
-            <MainFilters />
-        </Container>
+        <StyledSplashExplore>
+            <Container direction={"column"}>
+                <PageHeading text={"Explore Kupe"} style={{ marginTop: 0 }} />
+                <MainFilters />
+            </Container>
+        </StyledSplashExplore>
     )
 }
 
