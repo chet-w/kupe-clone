3fe985bdea6d7cdd19ec843168b26a66658536e1
Chet <chethana96@gmail.com>
2019-08-12 20:53:12 +1200

    added logic for disabling and enabling subtopic filter based on topic value

--- src/components/mainfilters.js
+++ src/components/mainfilters.js
@@ -1,29 +1,66 @@
-import React from 'react';
-// import { Select } from "antd";
-import { Form, Icon, Input, Button } from 'antd';
-// const { Option } =  Select;
+import React, { useState } from 'react';
+import { useStaticQuery, graphql } from "gatsby";
 
+import { Form, Select, Input, Button, AutoComplete } from 'antd';
+
+const { Option } = Select;
 
 const MainFilters = ({ form, formID }) => {
 
-    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = form;
+    const [ topicValue, setTopicValue ] = useState("");
+    const [ subtopicValue, setSubtopicValue ] = useState("");
+    const [ indicatorValue, setIndicatorValue ] = useState("");
 
-    // Only show error after a field is touched.
-    const usernameError = isFieldTouched('username') && getFieldError('username');
-    const passwordError = isFieldTouched('password') && getFieldError('password');
+    const allSearchables = useStaticQuery(graphql`
+        query allIndicators {
+            allIndicatorsJson {
+                nodes {
+                    topic
+                    subtopic
+                    shortDescription
+                }
+            }
+        }
+    `);
+
+    const topics = Array.from(new Set(allSearchables.allIndicatorsJson.nodes.map(node => node.topic))).sort();
+    const subtopics = Array.from(allSearchables.allIndicatorsJson.nodes.map(node => {
+        return {
+            topic: node.topic,
+            subtopic: node.subtopic
+        }
+    })).sort();
+    const indicators = allSearchables.allIndicatorsJson.nodes.map(node => {
+        return {
+            topic: node.topic,
+            subtopic: node.subtopic,
+            indicator: node.shortDescription
+        }
+    }).sort();
+
+    const handleTopicChange = value => {
+        setTopicValue(value);
+    };
+
+    const isSubtopicFieldDisabled = () => {
+        return !topics.includes(topicValue);
+    }
 
     return (
         <Form className="main-filters" layout="inline" onSubmit={() => console.log("submitted!")} id={formID || null}>
           <Form.Item className="filter-item">
-              <Input
-                prefix={"1. "}
+              <AutoComplete 
+                dataSource={topics}
                 placeholder="Choose a topic"
-              />,
+                onChange={e => handleTopicChange(e)}
+                filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
+              />
           </Form.Item>
           <Form.Item className="filter-item">
               <Input
                 prefix={"2. "}
                 placeholder="Choose a subtopic"
+                disabled={isSubtopicFieldDisabled()}
               />
           </Form.Item>
           <Form.Item className="filter-item">
