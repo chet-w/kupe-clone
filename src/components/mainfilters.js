2b77bdf92e573484e92b6a99dbce47a3c79d7f00
Chet <chethana96@gmail.com>
2019-08-13 18:45:09 +1200

    added link to located indicator based on input values

--- src/components/mainfilters.js
+++ src/components/mainfilters.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 import { Form, Select, Input, Button, AutoComplete } from 'antd';
+import { toPath } from '../lib/helpers';
 
 const { Option } = Select;
 
 const MainFilters = ({ form, formID }) => {
 
-    const [ topicValue, setTopicValue ] = useState("");
-    const [ subtopicValue, setSubtopicValue ] = useState("");
-    const [ indicatorValue, setIndicatorValue ] = useState("");
+    const [topicValue, setTopicValue] = useState("");
+    const [subtopicValue, setSubtopicValue] = useState("");
+    const [indicatorValue, setIndicatorValue] = useState("");
 
     const allSearchables = useStaticQuery(graphql`
         query allIndicators {
@@ -56,64 +57,69 @@ const MainFilters = ({ form, formID }) => {
 
     const isIndicatorFieldDisabled = () => {
         const currentSubtopics = Array.from(new Set(subtopics
-        .filter(subtopic => subtopic.topic === topicValue)
-        .map(subtopic => subtopic.subtopic)
+            .filter(subtopic => subtopic.topic === topicValue)
+            .map(subtopic => subtopic.subtopic)
         ));
 
         return !currentSubtopics
-        .includes(currentSubtopics
-            .find(subtopic => new RegExp(subtopic, "i").test(subtopicValue)));
+            .includes(currentSubtopics
+                .find(subtopic => new RegExp(subtopic, "i").test(subtopicValue)));
     };
 
     const isExploreButtonDisabled = () => {
         const currentIndicators = indicators
-        .filter(ind => ind.topic === topicValue &&
-             ind.subtopic === subtopicValue)
-        .map(ind => ind.indicator);
+            .filter(ind => ind.topic === topicValue &&
+                ind.subtopic === subtopicValue)
+            .map(ind => ind.indicator);
 
         return !currentIndicators
-        .includes(currentIndicators
-            .find(ind => new RegExp(ind.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), "i").test(indicatorValue)));
+            .includes(currentIndicators
+                .find(ind => new RegExp(ind.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), "i").test(indicatorValue)));
     }
 
     return (
         <Form className="main-filters" layout="inline" onSubmit={() => console.log("submitted!")} id={formID || null}>
-          <Form.Item className="filter-item">
-              <AutoComplete 
-                dataSource={topics}
-                placeholder="Choose a topic"
-                onChange={e => handleTopicChange(e)}
-                filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
-              />
-          </Form.Item>
-          <Form.Item className="filter-item">
-              <AutoComplete
-                dataSource={Array.from(new Set(subtopics.filter(sub => sub.topic === topicValue).map(sub => sub.subtopic))).sort()}
-                placeholder="Choose a subtopic"
-                onChange={e => handleSubtopicChange(e)}
-                disabled={isSubtopicFieldDisabled()}
-                filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
-              />
-          </Form.Item>
-          <Form.Item className="filter-item">
-              <AutoComplete
-                dataSource={indicators.filter(ind => ind.topic === topicValue && ind.subtopic === subtopicValue).map(ind => ind.indicator)}
-                placeholder="Choose an indicator"
-                onChange={e => handleIndicatorChange(e)}
-                disabled={isIndicatorFieldDisabled()}
-                filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
-              />
-          </Form.Item>
-          <Form.Item>
-            <Button
-             type="primary"
-             htmlType="submit"
-             disabled={isExploreButtonDisabled()}>
-              Explore
-            </Button>
-          </Form.Item>
+            <Form.Item className="filter-item">
+                <AutoComplete
+                    dataSource={topics}
+                    placeholder="Choose a topic"
+                    onChange={e => handleTopicChange(e)}
+                    filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
+                />
+            </Form.Item>
+            <Form.Item className="filter-item">
+                <AutoComplete
+                    dataSource={Array.from(new Set(subtopics.filter(sub => sub.topic === topicValue).map(sub => sub.subtopic))).sort()}
+                    placeholder="Choose a subtopic"
+                    onChange={e => handleSubtopicChange(e)}
+                    disabled={isSubtopicFieldDisabled()}
+                    filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
+                />
+            </Form.Item>
+            <Form.Item className="filter-item">
+                <AutoComplete
+                    dataSource={indicators.filter(ind => ind.topic === topicValue && ind.subtopic === subtopicValue).map(ind => ind.indicator)}
+                    placeholder="Choose an indicator"
+                    onChange={e => handleIndicatorChange(e)}
+                    disabled={isIndicatorFieldDisabled()}
+                    filterOption={(inputValue, option) => option.props.children.match(new RegExp(inputValue, "i"))}
+                />
+            </Form.Item>
+            <Form.Item>
+                <Link to={`${toPath(topicValue)}/${toPath(subtopicValue)}/${toPath(indicatorValue)}`}>
+                    <a>
+                        <Button
+                        type="primary"
+                        htmlType="submit"
+                        disabled={isExploreButtonDisabled()}
+                        >
+                        Explore
+                        </Button>
+                    </a>
+                </Link>
+            </Form.Item>
         </Form>
-      );
+    );
 }
 
 const Wrapped = Form.create({ name: 'horizontal_login' })(MainFilters);
