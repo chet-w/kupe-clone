74a86679bbe5cf3f8100a65012da1490c8133497
Chet <chethana96@gmail.com>
2019-09-08 12:23:16 +1200

    added data for popiulation table

--- src/components/methodpopulationtable.js
+++ src/components/methodpopulationtable.js
@@ -9,35 +9,96 @@ const MethodPopulationTable = () => {
         {
             key: 1,
             ethnicity: "Māori",
-            sex: "Male", 
+            sex: "Male",
             adultActual: 201,
             adultWeighted: 170,
             parentActual: 64,
             parentWeighted: 67,
-            render: (value, row, index) => {
-                const object = {
-                    children: value,
-                    props: {}
-                };
-                if(index === 0) {
-                    object.props.rowSpan = 2;
-                }
-                return object;
-            }
+            
+        },
+        {
+            key: 2,
+            ethnicity: "Māori",
+            sex: "Female",
+            adultActual: 362,
+            adultWeighted: 187,
+            parentActual: 172,
+            parentWeighted: 104,
+            
+        },
+        {
+            key: 3,
+            ethnicity: "Pacific",
+            sex: "Male",
+            adultActual: 164,
+            adultWeighted: 76,
+            parentActual: 63,
+            parentWeighted: 26,
+            
+        },
+        {
+            key: 4,
+            ethnicity: "Pacific",
+            sex: "Female",
+            adultActual: 306,
+            adultWeighted: 80,
+            parentActual: 168,
+            parentWeighted: 45,
+            
+        },
+        {
+            key: 5,
+            ethnicity: "Asian",
+            sex: "Male",
+            adultActual: 114,
+            adultWeighted: 201,
+            parentActual: 32,
+            parentWeighted: 50,
+            
+        },
+        {
+            key: 6,
+            ethnicity: "Asian",
+            sex: "Female",
+            adultActual: 131,
+            adultWeighted: 209,
+            parentActual: 43,
+            parentWeighted: 59,
+            
+        },
+        {
+            key: 7,
+            ethnicity: "European/Other",
+            sex: "Male",
+            adultActual: 571,
+            adultWeighted: 883,
+            parentActual: 91,
+            parentWeighted: 185,
+            
+        },
+        {
+            key: 8,
+            ethnicity: "European/Other",
+            sex: "Female",
+            adultActual: 876,
+            adultWeighted: 919,
+            parentActual: 194,
+            parentWeighted: 292,
+            
         }
     ];
 
     return (
         <Table dataSource={tableValues} pagination={false}>
             <Column title="Ethnicity (prioritised)" dataIndex="ethnicity" key="ethnicity" />
-            <Column title="Sex" dataIndex="sex" key="sex"/>
+            <Column title="Sex" dataIndex="sex" key="sex" />
             <ColumnGroup title="Adult">
-                <Column title="Actual" dataIndex="adultActual" key="adultActual"/>
-                <Column title="Weighted" dataIndex="adultWeighted" key="adultWeighted"/>
+                <Column title="Actual" dataIndex="adultActual" key="adultActual" />
+                <Column title="Weighted" dataIndex="adultWeighted" key="adultWeighted" />
             </ColumnGroup>
             <ColumnGroup title="Parent/caregiver">
-                <Column title="Actual" dataIndex="parentActual" key="parentActual"/>
-                <Column title="Weighted" dataIndex="parentWeighted" key="parentWeighted"/>
+                <Column title="Actual" dataIndex="parentActual" key="parentActual" />
+                <Column title="Weighted" dataIndex="parentWeighted" key="parentWeighted" />
             </ColumnGroup>
         </Table>
     )
